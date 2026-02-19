16a717a01c6abe04ac242bec44bb2326e8c81260
Angel Chen <angelchen7777@yahoo.com>
2026-02-19 15:51:51 -0500

    refactor: made harmonizing workflow into functions instead

--- 01_harmonize_landsat.R
+++ 01_harmonize_landsat.R
@@ -17,89 +17,117 @@ library(terra)
 # Create new folder to store rasters
 dir.create(path = file.path("harmonized_appeears_landsat_data"), showWarnings = F)
 
-# Get list of relevant tif files
-B01_files_v0 <- dir(file.path("appeears_landsat_data", "B01"), pattern = "B01", full.names = T)
-
 ## --------------------------------------------- ##
+#                   Function:
 #   Fixing rasters with different extents -----
 ## --------------------------------------------- ##
 
-# Create empty list for rasters that have a different extent
-diff_extent <- list()
-# For every tif file...
-for (i in seq_along(B01_files_v0)){
-  # Grab its metadata
-  size <- terra::describe(B01_files_v0[i])
-  # If the extent is different...
-  if (stringr::str_detect(size[3], "4171, 3839") == FALSE){
-    # Add to list
-    diff_extent <- append(diff_extent, B01_files_v0[i])
+fix_extent <- function(band_name){
+  
+  # Get list of relevant tif files
+  band_files_v0 <- dir(file.path("appeears_landsat_data", band_name), pattern = band_name, full.names = T)
+  
+  # Create empty list for rasters that have a different extent
+  diff_extent <- list()
+  # For every tif file...
+  for (i in seq_along(band_files_v0)){
+    # Grab its metadata
+    size <- terra::describe(band_files_v0[i])
+    # If the extent is different...
+    if (stringr::str_detect(size[3], "4171, 3839") == FALSE){
+      # Add to list
+      diff_extent <- append(diff_extent, band_files_v0[i])
+    }
   }
+  
+  # Get list of relevant tif files without the ones that have a different extent
+  band_files_v1 <- setdiff(band_files_v0, unlist(diff_extent))
+  # Read in one good raster to use as a template
+  template <- terra::rast(band_files_v1[1])
+  
+  message("Fixing raster extents")
+  
+  # Create empty list to store fixed rasters
+  fixed_rasters_list <- list()
+  # For every raster with a different extent...
+  for (i in seq_along(unlist(diff_extent))){
+    # Read in raster
+    wrong_extent <- terra::rast(unlist(diff_extent)[i])
+    # Fix extent 
+    # resample() will automatically divide by the scaling factor, so 
+    # multiply by the scaling factor to undo this
+    fixed_raster <- terra::resample(wrong_extent, template) * (0.0001)
+    # Add to list
+    fixed_rasters_list[[i]] <- fixed_raster
+  }
+  
+  # Combine fixed rasters into one SpatRaster
+  fixed_rasters <- terra::rast(fixed_rasters_list) 
+  # Fix varnames
+  varnames(fixed_rasters) <- names(fixed_rasters)
+  
+  # Combine the rasters that didn't need fixing into one SpatRaster
+  band <- terra::rast(band_files_v1)
+  
+  message("Combining all rasters")
+  
+  # Combine all rasters together
+  band_combined <- c(band, fixed_rasters)
+  # Sort raster layers by alphabetical names
+  band_combined_sorted <- terra::subset(band_combined, order(names(band_combined)))
+  # Fix varnames
+  varnames(band_combined_sorted) <- names(band_combined_sorted)
+  
+  message("Adding min/max statistics to metadata")
+  
+  # Add min/max statistics to metadata for completeness
+  setMinMax(band_combined_sorted)
 }
-
-# Get list of relevant tif files without the ones that have a different extent
-B01_files_v1 <- setdiff(B01_files_v0, unlist(diff_extent))
-# Read in one good raster to use as a template
-template <- terra::rast(B01_files_v1[1])
-
-# Create empty list to store fixed rasters
-fixed_rasters_list <- list()
-# For every raster with a different extent...
-for (i in seq_along(unlist(diff_extent))){
-  # Read in raster
-  wrong_extent <- terra::rast(unlist(diff_extent)[i])
-  # Fix extent 
-  # resample() will automatically divide by the scaling factor, so 
-  # multiply by the scaling factor to undo this
-  fixed_raster <- terra::resample(wrong_extent, template) * (0.0001)
-  # Add to list
-  fixed_rasters_list[[i]] <- fixed_raster
-}
-
-# Combine fixed rasters into one SpatRaster
-fixed_rasters <- terra::rast(fixed_rasters_list) 
-# Fix varnames
-varnames(fixed_rasters) <- names(fixed_rasters)
-
-# Combine rest of the rasters into one SpatRaster
-B01 <- terra::rast(B01_files_v1)
-
-# Combine every raster together
-B01_combined <- c(B01, fixed_rasters)
-# Sort raster layers by alphabetical names
-B01_combined_sorted <- terra::subset(B01_combined, order(names(B01_combined)))
-# Fix varnames
-varnames(B01_combined_sorted) <- names(B01_combined_sorted)
-# Add min/max statistics to metadata for completeness
-setMinMax(B01_combined_sorted)
 
 ## --------------------------------------------- ##
+#                   Function:
 #      Adding dates to raster metadata -----
 ## --------------------------------------------- ##
 
-# Extract the year and doy info
-year_doy <- stringr::str_extract(names(B01_combined_sorted), "[:digit:]{7}")
-
-# Create empty list to store formatted dates
-formatted_dates <- list()
-# For every extracted year and doy string...
-for (i in seq_along(year_doy)){
-  # Grab the year
-  year <- stringr::str_sub(year_doy[i], 1, 4)
-  # Grab the doy
-  doy <- as.numeric(stringr::str_sub(year_doy[i], 5, 7))
+add_dates <- function(band_name, harmonized_band){
+  # Extract the year and doy info
+  year_doy <- stringr::str_extract(names(harmonized_band), "[:digit:]{7}")
   
-  # Set the origin date as the start of the year
-  origin_date <- as.Date(paste0(year, "-01-01"))
-  # Convert doy to formatted date
-  formatted_date <- as.Date(doy - 1, origin = origin_date)
+  # Create empty list to store formatted dates
+  formatted_dates <- list()
+  # For every extracted year and doy string...
+  for (i in seq_along(year_doy)){
+    # Grab the year
+    year <- stringr::str_sub(year_doy[i], 1, 4)
+    # Grab the doy
+    doy <- as.numeric(stringr::str_sub(year_doy[i], 5, 7))
+    
+    # Set the origin date as the start of the year
+    origin_date <- as.Date(paste0(year, "-01-01"))
+    # Convert doy to formatted date
+    formatted_date <- as.Date(doy - 1, origin = origin_date)
+    
+    # Add to list
+    formatted_dates[[i]] <- formatted_date
+  }
   
-  # Add to list
-  formatted_dates[[i]] <- formatted_date
+  # Add formatted dates to metadata for completeness
+  time(harmonized_band) <- do.call("c", formatted_dates)
+  
+  message("Exporting raster, please be patient")
+  
+  # Export harmonized raster
+  terra::writeRaster(harmonized_band, 
+                     file.path("harmonized_appeears_landsat_data", paste0("ENP_Landsat_", band_name, ".tif")),
+                     overwrite = T)
 }
 
-# Add formatted dates to metadata for completeness
-time(B01_combined_sorted) <- do.call("c", formatted_dates)
+## --------------------------------------------- ##
+#               Harmonizing -----
+## --------------------------------------------- ##
 
-# Export harmonized raster
-terra::writeRaster(B01_combined_sorted, file.path("harmonized_appeears_landsat_data", "ENP_Landsat_B01.tif"))
+# Needed layers: "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B09", "B10", "B11"
+# Harmonize as needed
+
+B01_fix_extent <- fix_extent(band_name = "B01")
+add_dates(band_name = "B01", harmonized_band = B01_fix_extent)
