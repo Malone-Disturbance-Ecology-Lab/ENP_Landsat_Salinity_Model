a748b1aa6de4cf229e636e2432a5f1956b4b0ea1
Angel Chen <angelchen7777@yahoo.com>
2026-02-26 10:43:46 -0500

    refactor: exported salinity cont and grab data as CSV instead of RData

--- 01_harmonize_dbhydro.R
+++ 01_harmonize_dbhydro.R
@@ -112,8 +112,8 @@ DBHydro_df <- dplyr::bind_rows(cont_v2, grab_v1)
 #               Exporting -----
 ## --------------------------------------------- ##
 
-# Export combined cont and grab data
-save(DBHydro_df, file = "ENP_DBHydro_salinity_df.RData")
+# Export combined cont and grab data as CSV
+readr::write_csv(DBHydro_df, "ENP_DBHydro_salinity_df.csv")
 
 DBHydro_points_df <- DBHydro_df %>%
   # Select required columns
@@ -129,4 +129,4 @@ DBHydro_sf <- sf::st_as_sf(DBHydro_points_df, coords = c("longitude", "latitude"
 DBHydro_sf <- DBHydro_sf[sf::st_within(DBHydro_sf, ENP, sparse = FALSE), ]
 
 # Export distinct station points as shapefile
-sf::st_write(DBHydro_sf, file.path("ENP_DBHydro_sf", "ENP_DBHydro_sf.shp"))
+sf::st_write(DBHydro_sf, file.path("ENP_DBHydro_sf", "ENP_DBHydro_sf.shp"), append = FALSE)
