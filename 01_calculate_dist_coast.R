a47c7f2581bd4c1c1bc436c1ae8f09aa6ca4a516
Angel Chen <angelchen7777@yahoo.com>
2026-03-04 14:23:43 -0500

    refactor: made sure ENP boundary and coastline shapefile have the same CRS as template raster just in case

--- 01_calculate_dist_coast.R
+++ 01_calculate_dist_coast.R
@@ -30,14 +30,18 @@ enp <- sf::st_read(file.path(my_folder, "Everglades_NP_4326", "Everglades_NP_432
 # Read in one raster to use as a template 
 template_raster <- terra::rast(file.path(my_folder, "appeears_landsat_data", "B01", "HLSL30.020_B01_doy2013111_aid0001_17N.tif"))
 
+# Make the CRS of the ENP boundary shapefile the same as the template raster just in case
+enp_v2 <- enp %>%
+  sf::st_transform(sf::st_crs(template_raster))
+
 ## --------------------------------------------- ##
 #               Calculating -----
 ## --------------------------------------------- ##
 
 # Change CRS to match ENP shapefile
-ENP_coastline <- sf::st_transform(FL_coastline, crs = sf::st_crs(enp)) %>% 
+ENP_coastline <- sf::st_transform(FL_coastline, crs = sf::st_crs(enp_v2)) %>% 
   # Grab the coastline that intersects with ENP
-  sf::st_intersection(enp)
+  sf::st_intersection(enp_v2)
 
 # ggplot() +
 #   geom_sf(data = ENP_coastline)
@@ -45,7 +49,9 @@ ENP_coastline <- sf::st_transform(FL_coastline, crs = sf::st_crs(enp)) %>%
 # Convert to a linestring geometry
 ENP_coastline_line <- sf::st_cast(ENP_coastline, "LINESTRING") %>% 
   # Convert to terra object
-  terra::vect()
+  terra::vect() %>%
+  # Make sure it has the same CRS as our template raster
+  terra::project(terra::crs(template_raster))
 
 # terra::plot(ENP_coastline_line)
 
@@ -63,7 +69,7 @@ distance.Coast <- terra::distance(coastline, unit="m") # calculate distance of a
 names(distance.Coast) <- "distCoast"
 
 # Mask to ENP boundary
-distance.Coast.ENP <- terra::mask(distance.Coast, enp)
+distance.Coast.ENP <- terra::mask(distance.Coast, enp_v2)
 
 # Export
 terra::writeRaster(distance.Coast.ENP, file.path(my_folder, "ENP_DistCoast.tif"), overwrite = T)
