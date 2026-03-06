a961399a1e1089f681a34e2c04efa699928c1583
Angel Chen <angelchen7777@yahoo.com>
2026-03-06 16:14:10 -0500

    feat: incorporated meteorology data into extraction and harmonization script

--- 03_extract_and_harmonize_everything.R
+++ 03_extract_and_harmonize_everything.R
@@ -18,8 +18,8 @@ library(terra)
 library(sf)
 library(tsibble)
 
-# Point to Margo's Salinity Model folder
-salinity_folder <- file.path("/", "Volumes", "malonelab", "Research", "ENP_Salinity_Model") 
+# Point to the Landsat Salinity Model folder
+landsat_salinity_folder <- file.path("/", "Volumes", "malonelab", "Research", "ENP_Landsat_Salinity_Model") 
 
 # Read in Landsat files
 B01 <- terra::rast(file.path("harmonized_appeears_landsat_data", "ENP_Landsat_B01.tif"))
@@ -27,8 +27,15 @@ B02 <- terra::rast(file.path("harmonized_appeears_landsat_data", "ENP_Landsat_B0
 B03 <- terra::rast(file.path("harmonized_appeears_landsat_data", "ENP_Landsat_B03.tif"))
 B04 <- terra::rast(file.path("harmonized_appeears_landsat_data", "ENP_Landsat_B04.tif"))
 
-# Read in precip, solar radiation, temp files
+# Point to precip, solar radiation, temp files
+precip_files <- dir(file.path(landsat_salinity_folder, "ENP_Precipitation_Landsat_res"), pattern = ".tif", full.names = T)
+solar_rad_files <- dir(file.path(landsat_salinity_folder, "ENP_SolarRadiation_Landsat_res"), pattern = ".tif", full.names = T)
+temp_files <- dir(file.path(landsat_salinity_folder, "ENP_AverageTemperature_Landsat_res"), pattern = ".tif", full.names = T)
 
+# Read in one precip, solar rad, and temp raster to check
+test_precip_r <- terra::rast(precip_files[1])
+test_solar_rad_r <- terra::rast(solar_rad_files[1])
+test_temp_r <- terra::rast(temp_files[1])
 
 # Read in elevation, slope, distance to coast files
 ele_r <- terra::rast(file.path("ENP_Elevation.tif"))
@@ -43,13 +50,16 @@ DBHydro_sf_v2 <- DBHydro_sf %>%
   sf::st_transform(sf::st_crs(B01))
 
 # Make sure all CRS are the same as the Landsat
-terra::crs(B01) == terra::crs(DBHydro_sf_v2)
+terra::crs(B01) == terra::crs(test_precip_r)
+terra::crs(B01) == terra::crs(test_solar_rad_r)
+terra::crs(B01) == terra::crs(test_temp_r)
 terra::crs(B01) == terra::crs(ele_r)
 terra::crs(B01) == terra::crs(slope_r)
 terra::crs(B01) == terra::crs(dist_r)
+terra::crs(B01) == terra::crs(DBHydro_sf_v2)
 
 ## --------------------------------------------------------- ##
-#                 Extraction: Landsat -----
+#                Create Extraction Function -----
 ## --------------------------------------------------------- ##
 
 extract_from_raster <- function(raster, point_shapefile, new_column_name){
@@ -75,6 +85,11 @@ extract_from_raster <- function(raster, point_shapefile, new_column_name){
   return(r_points_long)
 }
 
+## --------------------------------------------------------- ##
+#                 Extraction: Landsat -----
+## --------------------------------------------------------- ##
+
+# Extract the Landsat data for the points
 B01_points <- extract_from_raster(raster = B01, 
                                   point_shapefile = terra::vect(DBHydro_sf_v2), 
                                   new_column_name = "B01")
@@ -105,9 +120,65 @@ landsat_points <- landsat_points_list %>%
 #       Extraction: Precip, Solar Radiation, Temp -----
 ## --------------------------------------------------------- ##
 
+# Precipitation -----------------------------------------------
 
+# Create an empty list to store extracted points
+precip_points_list <- list()
+# For every precip raster...
+for (i in 1:length(precip_files)){
+  # Read in the raster
+  precip_r <- terra::rast(precip_files[i])
+  # Extract the precip data for the points
+  extracted_precip_points <- extract_from_raster(raster = precip_r, 
+                                                 point_shapefile = terra::vect(DBHydro_sf_v2), 
+                                                 new_column_name = "precip")
+  # Save to list
+  precip_points_list[[i]] <- extracted_precip_points
+}
 
+# Combine all extracted points
+precip_points <- precip_points_list %>%
+  purrr::map_dfr(.f = select, everything())
 
+# Solar Radiation ---------------------------------------------
+
+# Create an empty list to store extracted points
+solar_rad_points_list <- list()
+# For every solar radiation raster...
+for (i in 1:length(solar_rad_files)){
+  # Read in the raster
+  solar_rad_r <- terra::rast(solar_rad_files[i])
+  # Extract the solar radiation data for the points
+  extracted_solar_rad_points <- extract_from_raster(raster = solar_rad_r, 
+                                                    point_shapefile = terra::vect(DBHydro_sf_v2), 
+                                                    new_column_name = "srad")
+  # Save to list
+  solar_rad_points_list[[i]] <- extracted_solar_rad_points
+}
+
+# Combine all extracted points
+solar_rad_points <- solar_rad_points_list %>%
+  purrr::map_dfr(.f = select, everything())
+
+# Average Temperature -----------------------------------------
+
+# Create an empty list to store extracted points
+temp_points_list <- list()
+# For every temperature raster...
+for (i in 1:length(temp_files)){
+  # Read in the raster
+  temp_r <- terra::rast(temp_files[i])
+  # Extract the temperature data for the points
+  extracted_temp_points <- extract_from_raster(raster = temp_r, 
+                                               point_shapefile = terra::vect(DBHydro_sf_v2), 
+                                               new_column_name = "tavg")
+  # Save to list
+  temp_points_list[[i]] <- extracted_temp_points
+}
+
+# Combine all extracted points
+temp_points <- temp_points_list %>%
+  purrr::map_dfr(.f = select, everything())
 
 ## --------------------------------------------------------- ##
 #    Extraction: Elevation, Slope, Distance to Coast -----
