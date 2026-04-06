3678fb976e9d35e86ad1b230998cf5ef7ab6d333
angelchen7 <angelchen7777@yahoo.com>
2026-04-06 15:27:35 -0400

    refactor: made vsurf script flexible to any specified interval

--- 04_model_vsurf.R
+++ 04_model_vsurf.R
@@ -9,7 +9,8 @@ library(caret)
 library(xgboost)
 library(parallel)
 
-# Create new folder to store results
+# Create new folders to store results
+dir.create(path = file.path("vsurf_4_years_results"), showWarnings = F)
 dir.create(path = file.path("vsurf_5_years_results"), showWarnings = F)
 
 DBSAL <- read_csv("DBSAL.csv", col_types = cols(formatted_date = col_date(format = "%Y-%m-%d"))) %>% 
@@ -35,17 +36,18 @@ DBSAL_v2 <- DBSAL %>%
 set.seed(77)
 
 #start_years <- c(2014, 2015, 2016, 2017)
-start_years <- c(2019)
+start_years <- c(2020)
+my_interval <- 5
 
 for (i in start_years) {
   
-  selected_interval <- paste0(i, "-", i+4)
+  selected_interval <- paste0(i, "-", i+my_interval-1)
   
   message(paste0("interval from: ", selected_interval))
   
-  # Subset to 5-year interval
+  # Subset to interval
   DBSAL_some_years <- DBSAL_v2 %>%
-    dplyr::filter(formatted_date >= as.Date(paste0(i,"-01-01")) & formatted_date < as.Date(paste0(i+5,"-01-01")))
+    dplyr::filter(formatted_date >= as.Date(paste0(i,"-01-01")) & formatted_date < as.Date(paste0(i+my_interval,"-01-01")))
   
   sample1 <- sample(1:nrow(DBSAL_some_years), 0.8*nrow(DBSAL_some_years)) 
   train_some_years <- DBSAL_some_years[sample1,] 
@@ -69,7 +71,7 @@ for (i in start_years) {
   
   # Predict for next 2 years
   DBSAL_next_years <- DBSAL_v2 %>%
-    dplyr::filter(formatted_date >= as.Date(paste0(i+5,"-01-01")) & formatted_date < as.Date(paste0(i+5+2,"-01-01")))
+    dplyr::filter(formatted_date >= as.Date(paste0(i+my_interval,"-01-01")) & formatted_date < as.Date(paste0(i+my_interval+2,"-01-01")))
   
   
   predictions2 <- predict(rf_vsurf, newdata = DBSAL_next_years, step = "pred")
@@ -79,7 +81,7 @@ for (i in start_years) {
   
   # Predict for next 3 years
   DBSAL_next_years <- DBSAL_v2 %>%
-    dplyr::filter(formatted_date >= as.Date(paste0(i+5,"-01-01")) & formatted_date < as.Date(paste0(i+5+3,"-01-01")))
+    dplyr::filter(formatted_date >= as.Date(paste0(i+my_interval,"-01-01")) & formatted_date < as.Date(paste0(i+my_interval+3,"-01-01")))
   
   
   predictions3 <- predict(rf_vsurf, newdata = DBSAL_next_years, step = "pred")
@@ -89,7 +91,7 @@ for (i in start_years) {
   
   # Predict for next 4 years
   DBSAL_next_years <- DBSAL_v2 %>%
-    dplyr::filter(formatted_date >= as.Date(paste0(i+5,"-01-01")) & formatted_date < as.Date(paste0(i+5+4,"-01-01")))
+    dplyr::filter(formatted_date >= as.Date(paste0(i+my_interval,"-01-01")) & formatted_date < as.Date(paste0(i+my_interval+4,"-01-01")))
   
   
   predictions4 <- predict(rf_vsurf, newdata = DBSAL_next_years, step = "pred")
@@ -99,7 +101,7 @@ for (i in start_years) {
   
   # Predict for next 5 years
   DBSAL_next_years <- DBSAL_v2 %>%
-    dplyr::filter(formatted_date >= as.Date(paste0(i+5,"-01-01")) & formatted_date < as.Date(paste0(i+5+5,"-01-01")))
+    dplyr::filter(formatted_date >= as.Date(paste0(i+my_interval,"-01-01")) & formatted_date < as.Date(paste0(i+my_interval+5,"-01-01")))
   
   
   predictions5 <- predict(rf_vsurf, newdata = DBSAL_next_years, step = "pred")
@@ -107,7 +109,7 @@ for (i in start_years) {
   r_squared5 <- 1 - sum((DBSAL_next_years$salinity - predictions5)^2) / sum((DBSAL_next_years$salinity - mean(DBSAL_next_years$salinity))^2)
   
   # Save results
-  result <-data.frame(interval_length = "5 years",
+  result <-data.frame(interval_length = paste0(my_interval, " years"),
                 years = selected_interval,
                 nrow_for_specified_years = nrow(DBSAL_some_years),
                 VSURF_vars = selected_vars,
@@ -119,5 +121,5 @@ for (i in start_years) {
   
   # Export as CSV
   readr::write_csv(result, 
-                   file.path("vsurf_5_years_results", paste0("vsurf_", selected_interval, ".csv")))
+                   file.path(paste0("vsurf_", my_interval, "_years_results"), paste0("vsurf_", selected_interval, ".csv")))
 }
