5376dab11c283d9968b3d24d3b75e9141743c3a6
angelchen7 <angelchen7777@yahoo.com>
2026-06-01 17:15:43 -0400

    feat: be more specific in filtering outliers and explored R^2 0.33 cutoff value

--- 05_model_validity_visualizations.R
+++ 05_model_validity_visualizations.R
@@ -16,28 +16,17 @@ library(randomForest)
 library(caret)
 library(rlang)
 
+# Create new folders to store results
 dir.create(path = file.path("model_validity_visualizations"), showWarnings = F)
+dir.create(path = file.path("model_validity_visualizations", "cutoff_0.5"), showWarnings = F)
+dir.create(path = file.path("model_validity_visualizations", "cutoff_0.33"), showWarnings = F)
 
-DBSAL_orig <- read_csv("DBSAL.csv", col_types = cols(formatted_date = col_date(format = "%Y-%m-%d")))  %>%
+DBSAL_orig <- read_csv("DBSAL.csv", col_types = cols(formatted_date = col_date(format = "%Y-%m-%d"))) %>%
   # Removing outliers to see general patterns more easily
   # Removing 4 outlier points where salinity had values of 3400, 1380, 157
-  dplyr::filter(salinity < 100) %>%
+  dplyr::filter(salinity < 100 | is.na(salinity)) %>%
   # Removing 1 outlier where CRSI is 8792
-  dplyr::filter(CRSI < 8000)
-
-DBSAL <- DBSAL_orig %>% 
-  # Remove missing values
-  na.omit() %>%
-  # Remove infinite values
-  dplyr::filter(!if_any(everything(), is.infinite))
-
-Fmask_lookup <- read_csv("HLSL30-020-Fmask-lookup.csv") %>%
-  # Find the Fmask values for cloudy days
-  dplyr::filter(Cloud == "Yes")
-
-DBSAL_v2 <- DBSAL %>%
-  # Filter out cloudy days
-  dplyr::filter(!(Fmask %in% Fmask_lookup$Value)) 
+  dplyr::filter(CRSI < 8000 | is.na(CRSI))
 
 # Create a version of the data with filled out band values
 # (Landsat and Sentinel values taken from the nearest previous day)
@@ -61,7 +50,9 @@ DBSAL_filled <- DBSAL_orig %>%
 #               Visualization -----
 ## --------------------------------------------- ##
 
-check <- DBSAL_filled %>%
+# Create plots when the R^2 cutoff to be considered "good" is 0.5 ---------
+
+check_0.5 <- DBSAL_filled %>%
   # Select relevant variables
   dplyr::select(station, formatted_date, grab, salinity, distCoast, slope, B03, srad, CRSI, SI, NDSI, B06, NLI, tavg) %>%
   # Denote "good" stations with a "1", "bad" stations with a "0"
@@ -76,11 +67,51 @@ vars <- c("salinity", "distCoast", "slope", "B03", "srad", "CRSI", "SI", "NDSI",
 # For each variable...
 for (a_var in vars){
   
+  check_0.5_v2 <- check_0.5 %>%
+    dplyr::select(formatted_date, station, !!sym(a_var), good) %>%
+    na.omit()
+  
   # Plot variable, facet by station
-  p <- ggplot(check, aes(x = formatted_date, y = !!sym(a_var), color = good)) +
+  p <- ggplot(check_0.5_v2, aes(x = formatted_date, y = !!sym(a_var), color = good)) +
     geom_line() +
     facet_wrap(~station)
   
   # Save plot
-  ggsave(file.path("model_validity_visualizations", paste0(a_var, "_plot.png")), p, height = 12, width = 18)
+  ggsave(file.path("model_validity_visualizations", "cutoff_0.5", paste0(a_var, "_plot.png")), p, height = 12, width = 18)
+}
+
+# Create plots when the R^2 cutoff to be considered "good" is 0.33 ---------
+
+good_stations <- c("2290930", "ENPCW", "ENPLO", "SEVENPALM", "TAYLORUPS",
+                   "ENPWP", "TAYLORS3", "TROUT CR_B", "FLAB37", "JOEBAY2E",
+                   "MCCORMICK", "MUD_CRKM", "ENPTC", "ENPTR", "FLAB29",
+                   "HIGHWAY_CR", "TTI57", "ENPGI", "FLAB30", "FLAB44",
+                   "TTI51B")
+
+check_0.33 <- DBSAL_filled %>%
+  # Select relevant variables
+  dplyr::select(station, formatted_date, grab, salinity, distCoast, slope, B03, srad, CRSI, SI, NDSI, B06, NLI, tavg) %>%
+  # Denote "good" stations with a "1", "bad" stations with a "0"
+  dplyr::mutate(good = dplyr::case_when(
+    station %in% good_stations ~ 1,
+    T ~ 0
+  ))
+
+# List relevant model variables
+vars <- c("salinity", "distCoast", "slope", "B03", "srad", "CRSI", "SI", "NDSI", "B06", "NLI", "tavg")
+
+# For each variable...
+for (a_var in vars){
+  
+  check_0.33_v2 <- check_0.33 %>%
+    dplyr::select(formatted_date, station, !!sym(a_var), good) %>%
+    na.omit()
+  
+  # Plot variable, facet by station
+  p <- ggplot(check_0.33_v2, aes(x = formatted_date, y = !!sym(a_var), color = good)) +
+    geom_line() +
+    facet_wrap(~station)
+  
+  # Save plot
+  ggsave(file.path("model_validity_visualizations", "cutoff_0.33", paste0(a_var, "_plot.png")), p, height = 12, width = 18)
 }
